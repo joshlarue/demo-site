5a99b33861ef7fefc57ab55d084c812d1642634b
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-08-30 21:46:56 -0600

    added text input/output functionality with js -- not yet working

---
+++ script.js
@@ -0,0 +1,9 @@
+// code is from mdn docs https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Functions
+// however, I am writing it from memory/as my own to try to grasp js concepts
+
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+input.addEventListener("keydown", (event) => {
+    output.textContent = `you pressed "${event.key}"`;
+});
